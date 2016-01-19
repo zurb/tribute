9c72c943c7abb385675ba9ead11e8a2de00d4035
Jordan Humphreys <jordan@mailyard.net>
2016-01-18 19:32:24 -0800

    Remove unecessary backwards compatibility.

--- dist/tribute.js
+++ dist/tribute.js
@@ -201,28 +201,16 @@ var TributeRange = function () {
           sel = undefined,
           range = undefined;
 
-      if (window.getSelection) {
-        sel = window.getSelection();
+      sel = window.getSelection();
 
-        if (sel.rangeCount) {
-          range = sel.getRangeAt(0);
+      if (sel.rangeCount) {
+        range = sel.getRangeAt(0);
 
-          if (range.commonAncestorContainer.parentNode == node) {
-            caretPos = range.endOffset;
-          }
-        }
-      } else if (document.selection && document.selection.createRange) {
-        range = document.selection.createRange();
-
-        if (range.parentElement() == node) {
-          var tempEl = document.createElement("span");
-          node.insertBefore(tempEl, node.firstChild);
-          var tempRange = range.duplicate();
-          tempRange.moveToElementText(tempEl);
-          tempRange.setEndPoint("EndToEnd", range);
-          caretPos = tempRange.text.length;
+        if (range.commonAncestorContainer.parentNode == node) {
+          caretPos = range.endOffset;
         }
       }
+
       return caretPos;
     }
   }]);
--- src/tribute.js
+++ src/tribute.js
@@ -175,33 +175,19 @@ class TributeRange {
   position(node) {
     let caretPos = 0, sel, range;
 
-    if (window.getSelection) {
-      sel = window.getSelection()
+    sel = window.getSelection()
 
-      if (sel.rangeCount) {
-        range = sel.getRangeAt(0)
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0)
 
-        if (range.commonAncestorContainer.parentNode == node) {
-          caretPos = range.endOffset
-        }
-      }
-    } else if (document.selection && document.selection.createRange) {
-      range = document.selection.createRange()
-
-      if (range.parentElement() == node) {
-        let tempEl = document.createElement("span")
-        node.insertBefore(tempEl, node.firstChild)
-        let tempRange = range.duplicate()
-        tempRange.moveToElementText(tempEl)
-        tempRange.setEndPoint("EndToEnd", range)
-        caretPos = tempRange.text.length
+      if (range.commonAncestorContainer.parentNode == node) {
+        caretPos = range.endOffset
       }
     }
+
     return caretPos
   }
 }
-
-
 
 let tribute = new Tribute({
   values: [
