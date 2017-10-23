c441c977f9f8347f581a584f8ff3eacc16394684
Dinesh Ramasamy <dinesh.vgp@gmail.com>
2017-10-23 14:32:11 +0530

    Fix: Show no match template

--- src/Tribute.js
+++ src/Tribute.js
@@ -222,6 +222,8 @@ class Tribute {
 
             let ul = this.menu.querySelector('ul')
 
+            this.range.positionMenuAtCaret(scrollTo)
+
             if (!items.length) {
                 let noMatchEvent = new CustomEvent('tribute-no-match', { detail: this.menu })
                 this.current.element.dispatchEvent(noMatchEvent)
@@ -250,8 +252,6 @@ class Tribute {
                 li.innerHTML = this.current.collection.menuItemTemplate(item)
                 ul.appendChild(li)
             })
-
-            this.range.positionMenuAtCaret(scrollTo)
         }
 
         if (typeof this.current.collection.values === 'function') {
