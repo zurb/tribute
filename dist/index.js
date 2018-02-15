9ad4c7486bdcd9208ccba5e8a0c65f6d69075f1b
Jordan Humphreys <jordan@mailyard.net>
2018-02-15 09:29:07 -0800

    Update build system to use bable-preset-env.

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Tribute = require("./Tribute");
+
+var _Tribute2 = _interopRequireDefault(_Tribute);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Tribute2.default; /**
+                                     * Tribute.js
+                                     * Native ES6 JavaScript @mention Plugin
+                                     **/
+
+module.exports = exports["default"];
