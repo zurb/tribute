8261319396b7291ad97cef165ad0b80d5c5738ea
PieterVerledens <5358512+PieterVerledens@users.noreply.github.com>
2018-08-22 15:44:31 +0200

    Update tributejs.d.ts

--- tributejs.d.ts
+++ tributejs.d.ts
@@ -1,4 +1,17 @@
-declare module 'tributejs' {
-    var Tribute: any;
+export as namespace tributejs;
+
+declare class Tribute{
+        constructor(element:Tribute.TributeOptions);
+    }
+
     export = Tribute;
+
+declare namespace Tribute {
+    export interface TributeOptions {
+        values: string[];
+        lookup: string;
+        menuItemTemplate:any;
+        selectClass: string;
+        selectTemplate: any;
+    }
 }
