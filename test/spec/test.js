e7a406a7172e00c6a22085853de86e0ad4ca6dd3
Nick Fisher <nick.fisher@avinium.com>
2019-04-11 13:28:43 +1000

    add fix+test for multi-char trigger when requireLeadingSpace is true

--- test/spec/test.js
+++ test/spec/test.js
@@ -13,4 +13,27 @@
       expect(tribute.collection[0].values).toEqual(options);
     });
   });
+  
+  describe('Multi-char trigger', function () {
+    it('lastIndexWithLeadingSpace should detect multi-char trigger correctly', () => {
+      const options = [
+        {key: 'Phil Heartman', value: 'pheartman'},
+        {key: 'Gordon Ramsey', value: 'gramsey'}
+      ];
+      const tribute = new Tribute({
+        values: options
+      });
+      const TributeRange = require('../../src/TributeRange.js');
+      var tributeRange = new TributeRange(tribute);
+      
+
+      expect(tributeRange.lastIndexWithLeadingSpace(" {{", "{{")).toEqual(1);
+      
+      expect(tributeRange.lastIndexWithLeadingSpace("{{", "{{")).toEqual(0);
+      expect(tributeRange.lastIndexWithLeadingSpace("a{{", "{{")).toEqual(-1);
+      expect(tributeRange.lastIndexWithLeadingSpace(" {", "{{")).toEqual(-1);
+    });
+  });
+  
 })();
+
