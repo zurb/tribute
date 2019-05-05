9c4c74eae432c42d79e13cb4e3a1bd55196406d9
MalayaliRobz <robinphilipthomas@outlook.in>
2019-05-06 00:33:40 +0530

    Revert "removing puppeteer and using chrome"
    This reverts commit fba4f2c7cd6f70c52cdc1efeafb5b99c2c0e9026.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,6 +2,7 @@
 // http://karma-runner.github.io/0.12/config/configuration-file.html
 // Generated on 2016-02-05 using
 // generator-karma 1.0.1
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
   'use strict';
