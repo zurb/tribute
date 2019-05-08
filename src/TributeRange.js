152a812b650ace56cf6c45b11cb4f8d1b4d36380
Frederik Hanghøj Iversen <fiversen@zendesk.com>
2019-05-08 10:35:09 +0200

    Fix issue with tribute being displayed in iframes

--- src/TributeRange.js
+++ src/TributeRange.js
@@ -179,8 +179,9 @@ class TributeRange {
     }
 
     getWindowSelection() {
-        if (this.tribute.collection.iframe) {
-            return this.tribute.collection.iframe.contentWindow.getSelection()
+        const {tribute: {current: {element: {ownerDocument}}}} = this
+        if (ownerDocument) {
+            return ownerDocument.getSelection()
         }
 
         return window.getSelection()
